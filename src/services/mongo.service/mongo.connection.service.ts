4484295e9a7cc159631d2f87ea7d951261332585
mkryuk <mkryuk@gmail.com>
2018-01-18 13:46:55 +0200

    modified mongodb connection

--- src/services/mongo.service/mongo.connection.service.ts
+++ src/services/mongo.service/mongo.connection.service.ts
@@ -8,16 +8,43 @@ export class MongoConnectionService {
     // plug in your own promise library instead: http://mongoosejs.com/docs/promises.html
     // https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/mongoose#promises
     (mongoose as any).Promise = global.Promise;
-    this.connection = mongoose.createConnection(connectionString);
-    this.connection
+    this.connection = mongoose.createConnection(connectionString, {
+      useMongoClient: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 500,
+      bufferMaxEntries: 0,
+    });
+    this.setupTriggers(this.connection);
+    // If the Node process ends, close the Mongoose connection
+    process.on("SIGINT", this.gracefulExit).on("SIGTERM", this.gracefulExit);
+  }
+
+  private setupTriggers(connection: mongoose.Connection) {
+    connection
       .once("open", () => {
         // tslint:disable-next-line:no-console
-        console.log(`mongodb connection created to ${connectionString}`);
+        console.log(`mongodb connection created to ${this.connectionString}`);
+      })
+      .on("disconnected", () => {
+        // tslint:disable-next-line:no-console
+        console.log("mongodb disconnected");
+      })
+      .on("reconnect", () => {
+        // tslint:disable-next-line:no-console
+        console.log("mongodb reconnected");
       })
       .on("error", (error: any) => {
         // tslint:disable-next-line:no-console
         console.warn(`Warning: ${error}`);
       });
+  }
+
+  private gracefulExit() {
+    this.connection.close(() => {
+      // tslint:disable-next-line:no-console
+      console.log(`Mongoose connection :${this.connectionString} is disconnected through app termination`);
+      process.exit(0);
+    });
   }
 }
 
